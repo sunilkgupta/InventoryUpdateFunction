000d485238bf43222723093b85ebe8107c2bbe7f
Sunil Kumar <sunilkgupta86@gmail.com>
2024-10-06 10:23:24 +0530

    Updated

---
+++ InventoryUpdateFunction/Constants.cs
@@ -0,0 +1,22 @@
+﻿
+namespace InventoryUpdateFunction
+{
+    /// <summary>
+    /// Constants 
+    /// </summary>
+    public static class Constants
+    {
+        #region Important constants
+        public const string SP_Name = "UpdateInventoryUponNewOrder";
+        public const string Tbl_Name = "[dbo].[Order]";
+        public const string ConnString_Config = "DbConnection";
+        public const string Function_Name = "DbSyncTrigger";
+        public const string Input_id = "@Id";
+        public const string Input_Quantity = "@Quantity";
+        #endregion
+
+        #region log messages constants
+
+        #endregion
+    }
+}
--- InventoryUpdateFunction/InventoryOrderSync.cs
+++ InventoryUpdateFunction/InventoryOrderSync.cs
@@ -9,52 +9,53 @@ using System.Data;
 
 namespace InventoryUpdateFunction
 {
+    /// <summary>
+    /// Function gets fire immediate whenever there is change/add in Order tbl.
+    /// </summary>
     public static class InventoryOrderSync
     {
-        [FunctionName("DbSyncTrigger")]
-        public static void Run([SqlTrigger("[dbo].[Order]", "DbConnection")]
-            IReadOnlyList<SqlChange<Order>> changes,
-            ILogger log)
+        [FunctionName(Constants.Function_Name)]
+        public static void Run([SqlTrigger(Constants.Tbl_Name, Constants.ConnString_Config)]
+            IReadOnlyList<SqlChange<Order>> changes, ILogger log)
         {
             log.LogInformation("SQL Changes: " + JsonConvert.SerializeObject(changes));
 
             var str = Environment.GetEnvironmentVariable("DbConnection");
-            using (var conn = new SqlConnection(str))
+            using var conn = new SqlConnection(str);
+            conn.Open();
+            try
             {
-                conn.Open();
-                try
-                {
-                    var sp_Name = "UpdateInventoryUponNewOrder";
+                var sp_Name = Constants.SP_Name;
 
-                    using (var cmd = new SqlCommand(sp_Name, conn))
-                    {
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.Add(new SqlParameter("@Id", changes[0].Item.ItemId));
-                        cmd.Parameters.Add(new SqlParameter("@Quantity", changes[0].Item.Quantity));
-                        cmd.ExecuteNonQuery();
-                        log.LogInformation($"Inventory updated successfully");
-                    }
-                }
-                catch (Exception ex)
+                using (var cmd = new SqlCommand(sp_Name, conn))
                 {
-                    log.LogError(ex.Message, ex);
-                    throw;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter(Constants.Input_id, changes[0].Item.ItemId));
+                    cmd.Parameters.Add(new SqlParameter(Constants.Input_Quantity, changes[0].Item.Quantity));
+                    cmd.ExecuteNonQuery();
+                    log.LogInformation($"Inventory updated successfully");
                 }
-                finally
-                {
-                    conn.Close();
-                    log.LogInformation("SQL Cconnection closed!");
-                    log.LogInformation("SQL Changes completed!");
-
-                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+                log.LogInformation("SQL Cconnection closed!");
+                log.LogInformation("SQL Changes completed!");
             }
         }
     }
 
+    /// <summary>
+    /// Object as Order class model
+    /// </summary>
     public class Order
     {
         public Guid OrderId { get; set; }
-
         public string? CustomerName { get; set; }
         public Guid ItemId { get; set; }
         public int Quantity { get; set; }
